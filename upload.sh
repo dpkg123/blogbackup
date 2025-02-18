c2998a8335a8728424aeda0b391cefc88ba15c85
dpkg123 <test1@qw5d.onmicrosoft.com>
2025-02-18 16:36:51 +0000

    Site source uploaded: Tue Feb 18 04:36:51 PM UTC 2025

--- upload.sh
+++ upload.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 set +e
-git pull --rebase
 git add .
 git commit -m "Site source uploaded: $(LANG=en_US.UTF-8 date)"
-git push -u origin +main
+git pull --rebase && git push -u origin +main
